74c28e2a0ff379b604f7a9949ae08946df511522
Yupeng Chen <cyp0633@cyp0633.icu>
2023-12-04 17:08:40 +0800

    day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,4 +1,5 @@
 advent_of_code::solution!(2);
+use std::cmp;
 
 pub fn part_one(input: &str) -> Option<u32> {
     let mut id_sum: u32 = 0;
@@ -7,7 +8,7 @@ pub fn part_one(input: &str) -> Option<u32> {
         let content = game.split(": ").collect::<Vec<&str>>()[1];
         // split "X blue, Y red; XX green, YY yellow"
         let gotchas = content.split("; ").collect::<Vec<&str>>();
-        let (mut red,mut green,mut blue) = (0,0,0);
+        let (mut red, mut green, mut blue) = (0, 0, 0);
         // Per gotcha: maximum 12 red, 13 green, 14 blue
         for gotcha in gotchas {
             let colors = gotcha.split(", ").collect::<Vec<&str>>();
@@ -33,7 +34,32 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut id_sum: u32 = 0;
+    for game in input.lines() {
+        // split "Game X: yyyyyyyyy"
+        let content = game.split(": ").collect::<Vec<&str>>()[1];
+        // split "X blue, Y red; XX green, YY yellow"
+        let gotchas = content.split("; ").collect::<Vec<&str>>();
+        let (mut red, mut green, mut blue) = (0, 0, 0);
+        // Get the maximum of each color
+        for gotcha in gotchas {
+            let colors = gotcha.split(", ").collect::<Vec<&str>>();
+            for color in colors {
+                // color: "X blue/green/red"
+                let color = color.split(" ").collect::<Vec<&str>>();
+                let count = color[0].parse::<u32>().unwrap();
+                match color[1] {
+                    "blue" => blue = cmp::max(blue, count),
+                    "green" => green = cmp::max(green, count),
+                    "red" => red = cmp::max(red, count),
+                    _ => panic!("Unknown color: {}", color[1]),
+                }
+            }
+        }
+        let color_pow = red * green * blue;
+        id_sum += color_pow;
+    }
+    Some(id_sum)
 }
 
 #[cfg(test)]
@@ -49,6 +75,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2286));
     }
 }
