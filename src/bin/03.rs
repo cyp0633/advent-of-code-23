5c7685296801fe785d038bb007218ae8eb1e6881
cyp0633 <cyp0633@cyp0633.icu>
2023-12-05 22:39:38 +0800

    day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -67,8 +67,163 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
+pub fn part_two(input: &str) -> Option<u32> {
+    // convert lines into a char matrix
+    let lines = input.lines();
+    let mut sum = 0;
+    let mut matrix: Vec<Vec<char>> = Vec::new();
+    for line in lines {
+        matrix.push(line.chars().collect());
+    }
+
+    for (x, line) in matrix.iter().enumerate() {
+        for (y, ch) in line.iter().enumerate() {
+            if *ch == '*' {
+                // find numbers nearby
+                let (
+                    mut up_left,
+                    mut up,
+                    mut up_right,
+                    mut left,
+                    mut right,
+                    mut down_left,
+                    mut down,
+                    mut down_right,
+                ) = (false, false, false, false, false, false, false, false);
+                let mut pow = 1;
+                if x > 0 {
+                    if y > 0 {
+                        if is_digit(matrix[x - 1][y - 1]) {
+                            up_left = true;
+                        }
+                    }
+                    if is_digit(matrix[x - 1][y]) {
+                        up = true;
+                    }
+                    if y < matrix[x].len() - 1 {
+                        if is_digit(matrix[x - 1][y + 1]) {
+                            up_right = true;
+                        }
+                    }
+                }
+                if y > 0 && is_digit(matrix[x][y - 1]) {
+                    left = true;
+                }
+                if y < matrix[x].len() - 1 && is_digit(matrix[x][y + 1]) {
+                    right = true;
+                }
+                if x < matrix.len() - 1 {
+                    if y > 0 {
+                        if is_digit(matrix[x + 1][y - 1]) {
+                            down_left = true;
+                        }
+                    }
+                    if is_digit(matrix[x + 1][y]) {
+                        down = true;
+                    }
+                    if y < matrix[x].len() - 1 {
+                        if is_digit(matrix[x + 1][y + 1]) {
+                            down_right = true;
+                        }
+                    }
+                }
+                // get number count
+                let mut count = 0;
+                match (up_left, up, up_right) {
+                    (false, false, true) | (true, false, false) | (false, true, false) => {
+                        count += 1
+                    }
+                    (true, true, false) | (false, true, true) => {
+                        count += 1;
+                        up = false; // to prevent calculating value twice
+                    }
+                    (true, false, true) => count += 2,
+                    (true, true, true) => {
+                        count += 1;
+                        up_left = false; // as above
+                        up_right = false;
+                    }
+                    (false, false, false) => {}
+                }
+                if left {
+                    count += 1;
+                }
+                if right {
+                    count += 1;
+                }
+                match (down_left, down, down_right) {
+                    (false, false, true) | (true, false, false) | (false, true, false) => {
+                        count += 1
+                    }
+                    (true, true, false) | (false, true, true) => {
+                        count += 1;
+                        down = false;
+                    }
+                    (true, false, true) => count += 2,
+                    (true, true, true) => {
+                        count += 1;
+                        down_left = false;
+                        down_right = false;
+                    }
+                    (false, false, false) => {}
+                }
+                println!("({}, {}) has {} numbers", x, y, count);
+                if count != 2 {
+                    // not a "gear"
+                    continue;
+                }
+                if up_left {
+                    pow *= get_value(&matrix[x - 1], y - 1);
+                }
+                if up {
+                    pow *= get_value(&matrix[x - 1], y);
+                }
+                if up_right {
+                    pow *= get_value(&matrix[x - 1], y + 1);
+                }
+                if left {
+                    pow *= get_value(&matrix[x], y - 1);
+                }
+                if right {
+                    pow *= get_value(&matrix[x], y + 1);
+                }
+                if down_left {
+                    pow *= get_value(&matrix[x + 1], y - 1);
+                }
+                if down {
+                    pow *= get_value(&matrix[x + 1], y);
+                }
+                if down_right {
+                    pow *= get_value(&matrix[x + 1], y + 1);
+                }
+                sum += pow;
+                println!("Adding {} at ({}, {})", pow, x, y);
+            }
+        }
+    }
+    Some(sum as u32)
+}
+
 fn is_symbol(char: char) -> bool {
     (char < '0' || char > '9') && char != '.'
+}
+
+fn is_digit(char: char) -> bool {
+    char >= '0' && char <= '9'
+}
+
+// Since the value stays only on one line, we can just use one line.
+fn get_value(matrix: &Vec<char>, y: usize) -> i32 {
+    let mut start_y = y;
+    let mut val = 0;
+    while start_y > 0 && is_digit(matrix[start_y - 1]) {
+        start_y -= 1;
+    }
+    while start_y < matrix.len() && is_digit(matrix[start_y]) {
+        val = val * 10 + (matrix[start_y] as i32 - '0' as i32);
+        start_y += 1;
+    }
+    val
 }
 
 #[cfg(test)]
@@ -84,6 +239,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
